e17cd81b53873c06f55a62ea11130118c856107d
epicmo <1982742309@qq.com>
2022-10-02 19:37:35 +0800

    First commit

---
+++ cmake-build-debug/test/CMakeFiles/YggdrasilServer_test.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/epicmo/桌面/Yggdrasil/YggdrasilServer/test/test_main.cc" "test/CMakeFiles/YggdrasilServer_test.dir/test_main.cc.o" "gcc" "test/CMakeFiles/YggdrasilServer_test.dir/test_main.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/test/CMakeFiles/YggdrasilServer_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/YggdrasilServer_test.dir/test_main.cc.o"
+  "CMakeFiles/YggdrasilServer_test.dir/test_main.cc.o.d"
+  "YggdrasilServer_test"
+  "YggdrasilServer_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/YggdrasilServer_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
